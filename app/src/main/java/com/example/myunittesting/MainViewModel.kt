43724c62595d569f28aa7751c60f46c2c59d492b
zilfanafalahi <zilfanafalahi45@gmail.com>
2020-07-08 20:51:11 +0700

    Testing Espresso

---
+++ app/src/main/java/com/example/myunittesting/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.myunittesting
+
+internal class MainViewModel(private val cuboidModel: CuboidModel) {
+    val circumference: Double
+        get() = cuboidModel.circumference
+
+    val surfaceArea: Double
+        get() = cuboidModel.surfaceArea
+
+    val volume: Double
+        get() = cuboidModel.volume
+
+    fun save(l: Double, w: Double, h: Double) {
+        cuboidModel.save(l, w, h)
+    }
+}
